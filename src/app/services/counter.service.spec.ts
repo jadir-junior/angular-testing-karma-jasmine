867f09ce5a8380727cc8395184c6784d49794b82
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-02-02 10:14:17 -0300

    testing a service with internat state

--- src/app/services/counter.service.spec.ts
+++ src/app/services/counter.service.spec.ts
@@ -1,15 +1,44 @@
 import { CounterService } from './counter.service';
-import { TestBed } from '@angular/core/testing';
+import { first } from 'rxjs';
 
 describe('CounterService', () => {
   let service: CounterService;
 
+  function expectCount(count: number): void {
+    let actualCount: number | undefined;
+    service
+      .getCount()
+      .pipe(first())
+      .subscribe((actualCount2) => {
+        actualCount = actualCount2;
+      });
+    expect(actualCount).toBe(count);
+  }
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CounterService);
+    service = new CounterService();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('returns the count', () => {
+    expectCount(0);
+  });
+
+  it('increments the count', () => {
+    service.increment();
+
+    expectCount(1);
+  });
+
+  it('decrements the count', () => {
+    service.decrement();
+
+    expectCount(-1);
+  });
+
+  it('resets the count', () => {
+    const newCount = 123;
+    service.reset(newCount);
+
+    expectCount(123);
   });
 });
--- src/app/services/counter.service.ts
+++ src/app/services/counter.service.ts
@@ -2,10 +2,17 @@ import { BehaviorSubject, Observable } from 'rxjs';
 
 import { Injectable } from '@angular/core';
 
+interface ICounter {
+  getCount(): Observable<number>;
+  increment(): void;
+  decrement(): void;
+  reset(newCount: number): void;
+}
+
 @Injectable({
   providedIn: 'root',
 })
-export class CounterService {
+export class CounterService implements ICounter {
   private count = 0;
 
   private subject: BehaviorSubject<number>;
